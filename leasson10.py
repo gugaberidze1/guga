fdfacfa04b81a06e4ce61990b6429757cc2b9fff
gugaberidze1 <145611522+gugaberidze1@users.noreply.github.com>
2023-10-30 00:07:03 +0400

    Add files via upload

---
+++ leasson10.py
@@ -0,0 +1,15 @@
+
+x = [2, 4, 6, 2, 4, 7, 2, 9]
+for i in range(2):
+     x.remove(4)
+print(x)
+
+
+fam =[37, 39, 15]
+
+full_sentence = "mom age is:{}, dad age is:{}, my age is:{}".format(fam[0],fam[1],fam[2])
+print(full_sentence)
+
+aftertenyears = [37 + 10, 39 + 10, 15 + 10]
+fulsent = "mom age in 10 years:{},dad age in 10 years:{},my age in 10 years:{}".format(aftertenyears[0],aftertenyears[1],aftertenyears[2])
+print(fulsent)
