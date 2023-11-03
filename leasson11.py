b80542cfaf57836f247938e5c13183d4dbe53336
gugaberidze1 <145611522+gugaberidze1@users.noreply.github.com>
2023-11-03 19:33:37 +0400

    Add files via upload

---
+++ leasson11.py
@@ -0,0 +1,13 @@
+
+def samkutxedis_gverdebi(d, e , f):
+   
+    if d+e > f:
+
+        print("ასეთი სამკუთხედი ვერ შედგება")
+
+    if d+e < f:
+        print("ასეთი სამკუთხედი შედგება")
+
+samkutxedis_gverdebi(20 , 40 , 80)
+
+
